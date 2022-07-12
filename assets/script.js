accacec75e43c24bcb4d7a7fb044a8d5f2ceab8f
Combatxd <logan.schoerner@gmail.com>
2022-07-12 13:46:35 -0600

    storedquestions

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,37 @@
-var startButton = document.querySelector("#start-btn")
-var questions = document.querySelector("#questionContainer")
+var startButton = document.querySelector("#start-btn");
+var questionContain = document.querySelector("#questionContainer");
+
+var questionsAnswers = [
+    {
+       title: "this is question 1",
+       options: ["op 1", "op2", "op3", "op4"],
+       correct: "op3", 
+    },
+    {
+        title: "this is question 2",
+        options: ["op 1", "op2", "op3", "op4"],
+        correct: "op1",
+    },
+    {
+        title: "this is question 3",
+        options: ["op 1", "op2", "op3", "op4"],
+        correct: "op4",
+    },
+    {
+        title: "this is question 4",
+        options: ["op 1", "op2", "op3", "op4"],
+        correct: "op2",
+    },
+    {
+        title: "this is question 5",
+        options: ["op 1", "op2", "op3", "op4"],
+        correct: "op1",
+    },
+]
 
 function startquiz() {
-    questions.style.visibility = "visible";
-    var sec = 60;
+    questionContain.style.visibility = "visible";
+    var sec = 150;
     startButton.disabled = true;
     var timer = setInterval(function(){
         document.getElementById("timer").innerHTML= sec + " sec";
@@ -12,7 +40,7 @@ function startquiz() {
             clearInterval(timer);
         }
     }, 1000);
-    
+   
 }
 
 
